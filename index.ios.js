d88bbe759c9fa7f6dfb6398faa33fa449e1e65ae
Valdeci Gomes <coderade@outlook.com>
2017-09-05 23:38:24 -0300

    Fix index.ios.js import error.

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
+import {StackNavigator} from 'react-navigation';
 import ReactCourses from "./src/components/ReactCourses";
 import NativeCourses from "./src/components/NativeCourses";
 
